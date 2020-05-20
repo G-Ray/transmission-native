8e526712d9cedb8647c80f0607f2e9f6d8c4a25a
G-Ray <geoffrey.bonneville@gmail.com>
2020-05-20 21:21:24 +0200

    Set self in a set to avoid GC. Thanks @mafintosh !

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const tr = require('./binding')
 
 class Transmission {
+  constructor () {
+    this.set = new Set()
+  }
+
   init () {
     tr.sessionInit()
   }
@@ -11,10 +15,12 @@ class Transmission {
 
   request (reqJson, cb) {
     const self = Buffer.alloc(tr.sizeof_tr_napi_t)
+    this.set.add(self)
     tr.request(self, Buffer.from(JSON.stringify(reqJson)), (err, res) => {
       if (err) return cb(err)
       const resJson = JSON.parse(res)
       const e = (resJson.result !== 'success') ? new Error(resJson.result) : null
+      this.set.delete(self)
       cb(e, resJson)
     })
   }
