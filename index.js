7cae71edb635f9d3e40f1c6de82925cb0135e1b4
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-02-24 11:29:49 +0100

    Also return promise for request function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const util = require('util')
+
 const tr = require('./binding')
 
 class Transmission {
   constructor (configDir = 'transmission', appName = 'transmission') {
+    // Keep reference of requests in a set to avoid the requests to be GC
     this.set = new Set()
     // NOTE: we append the buffer with a null character to delimitate the C string
     tr.sessionInit(Buffer.from(configDir + '\0'), Buffer.from(appName + '\0'))
@@ -12,6 +15,17 @@ class Transmission {
   }
 
   request (reqJson, cb) {
+    // Callback api
+    if (cb && typeof cb === 'function') {
+      return this._request(reqJson, cb)
+    }
+
+    // Promise api
+    const promisified = util.promisify(this._request).bind(this)
+    return promisified(reqJson)
+  }
+
+  _request (reqJson, cb) {
     const self = Buffer.alloc(tr.sizeof_tr_napi_t)
     this.set.add(self)
     tr.request(self, Buffer.from(JSON.stringify(reqJson)), (err, res) => {
